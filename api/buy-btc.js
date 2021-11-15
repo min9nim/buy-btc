aa17200f6296d3b8b35c34673eebdd22e3049d44
keating <mgsong@madup.com>
2021-11-15 15:54:30 +0900

    add healthCheck option on buy-btc

--- api/buy-btc.js
+++ api/buy-btc.js
@@ -3,13 +3,18 @@ const request = require('../src/utils/req')
 const axios = require('axios')
 
 const handler = async (req, res) => {
-  const myIp = await axios({
+  const {data: myIp} = await axios({
     method: 'get',
     url: 'https://echo-api.vercel.app/api',
   })
   console.log('myIp', myIp)
 
-  const { accessKey, secretKey } = req.body
+  const { accessKey, secretKey, healthCheck } = req.body
+
+  if(healthCheck){
+    res.json(myIp)
+    return
+  }
 
   const result = await request({
     method: 'post',
