b3209e0c8b36e01b801cd9ec3931ee9d9bb66050
keating <min9nim>
2021-12-03 16:56:06 +0900

    update error log

--- src/utils/handle-error.js
+++ src/utils/handle-error.js
@@ -2,7 +2,8 @@ module.exports = fn => async (req, res) => {
   try{
     return await fn(req, res)
   }catch (e){
-    console.error(e)
+    // console.error(e)
+    console.error(e.message)
     res.status(401).json({ message:e.message })
   }
 }
