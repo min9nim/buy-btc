2c31d750bdaf6751e98f98359ae3394e405288e4
Keating <min9nim@gmail.com>
2022-02-22 15:27:41 +0900

    update

--- api/fear-greed-index.js
+++ api/fear-greed-index.js
@@ -3,12 +3,13 @@ const handleError = require('../src/utils/handle-error')
 const axios = require('axios')
 
 const handler = async (req, res) => {
-    const result = await axios({
-        method: 'post',
-        url: 'https://alternative.me/api/crypto/fear-and-greed-index/history',
-        data: {days: 100}
-    })
-    res.json(result.data)
+  const { days = 100 } = req.query
+  const result = await axios({
+    method: 'post',
+    url: 'https://alternative.me/api/crypto/fear-and-greed-index/history',
+    data: { days },
+  })
+  res.json(result.data)
 }
 
 module.exports = allowCors(handleError(handler))
