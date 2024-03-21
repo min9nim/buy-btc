e5c737e30a4785cf39cb6cf9e8b8c276bf30e347
keating <min9nim@gmail.com>
2024-03-21 22:14:13 +0900

    new /api/my-accounts-bithumb

---
+++ api/my-accounts-bithumb.js
@@ -0,0 +1,18 @@
+const allowCors = require('../src/utils/cors')
+const handleError = require('../src/utils/handle-error')
+const { XCoinAPI } = require('../src/utils/XCoinAPI')
+
+const handler = async (req, res) => {
+  const { accessKey, secretKey } = req.query
+
+  const xcoinAPI = new XCoinAPI(accessKey, secretKey)
+  const rgParams = {
+    order_currency: 'BTC',
+    payment_currency: 'KRW',
+  }
+  const res = await xcoinAPI.xcoinApiCall('/info/account', rgParams)
+
+  res.json(res.body)
+}
+
+module.exports = allowCors(handleError(handler))
