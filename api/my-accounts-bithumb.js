9bc2dc6074cf898e1b2a3dada7ac466e3742f924
keating <min9nim@gmail.com>
2024-03-21 22:36:55 +0900

    add usdt

--- api/my-accounts-bithumb.js
+++ api/my-accounts-bithumb.js
@@ -6,16 +6,23 @@ const handler = async (req, res) => {
   const { accessKey, secretKey } = req.query
 
   const xcoinAPI = new XCoinAPI(accessKey, secretKey)
-  const [btc, krw] = await Promise.all([
+  const [btc, krw, usdt] = await Promise.all([
     xcoinAPI.xcoinApiCall('/info/balance', {
       currency: 'BTC',
     }),
     xcoinAPI.xcoinApiCall('/info/balance', {
       currency: 'KRW',
     }),
+    xcoinAPI.xcoinApiCall('/info/balance', {
+      currency: 'USDT',
+    }),
   ])
 
-  res.json({ btc: JSON.parse(btc.body), krw: JSON.parse(krw.body) })
+  res.json({
+    btc: JSON.parse(btc.body),
+    krw: JSON.parse(krw.body),
+    usdt: JSON.parse(usdt.body),
+  })
 }
 
 module.exports = allowCors(handleError(handler))
