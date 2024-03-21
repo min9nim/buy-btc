13de275ccd349c9ceb785720785a8a952ea3c159
keating <min9nim@gmail.com>
2024-03-21 22:40:32 +0900

    refactor

--- api/my-accounts-bithumb.js
+++ api/my-accounts-bithumb.js
@@ -3,26 +3,14 @@ const handleError = require('../src/utils/handle-error')
 const { XCoinAPI } = require('../src/utils/XCoinAPI')
 
 const handler = async (req, res) => {
-  const { accessKey, secretKey } = req.query
+  const { accessKey, secretKey, currency } = req.query
 
   const xcoinAPI = new XCoinAPI(accessKey, secretKey)
-  const [btc, krw, usdt] = await Promise.all([
-    xcoinAPI.xcoinApiCall('/info/balance', {
-      currency: 'BTC',
-    }),
-    xcoinAPI.xcoinApiCall('/info/balance', {
-      currency: 'KRW',
-    }),
-    xcoinAPI.xcoinApiCall('/info/balance', {
-      currency: 'USDT',
-    }),
-  ])
-
-  res.json({
-    btc: JSON.parse(btc.body),
-    krw: JSON.parse(krw.body),
-    usdt: JSON.parse(usdt.body),
+  const result = await xcoinAPI.xcoinApiCall('/info/balance', {
+    currency,
   })
+
+  res.json(JSON.parse(result.body))
 }
 
 module.exports = allowCors(handleError(handler))
